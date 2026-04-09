770d3ebc85ed9f5a7dcffcb6d119bb374e6250de
GaoXuGuang <gxp@example.com>
2026-04-09 20:58:03 +0800

    chore: remove functions dir, use only Next.js App Router

--- functions/api/auth/logout.ts
+++
@@ -1,5 +0,0 @@
-export async function onRequestGet(context) {
-  const response = Response.redirect("https://remove-bakg-img.bond/", 302);
-  response.headers.set("Set-Cookie", "session=; HttpOnly; Secure; SameSite=Lax; Path=/; Max-Age=0");
-  return response;
-}
